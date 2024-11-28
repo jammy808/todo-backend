318894ba9beb84c1e5ac8715f98235822544c42b
Soham <jambhwadekarsoham@gmail.com>
2024-11-28 22:54:20 +0530

    cors update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: 'https://todo-frontend-two-beta.vercel.app/',
+        origin: 'https://todo-frontend-two-beta.vercel.app',
         credentials: true, 
     })
 );
